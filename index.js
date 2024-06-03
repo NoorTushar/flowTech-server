67134b03726235925a231620f826529d05ee3060
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-03 18:12:04 +0600

    PUT Method to save a user on database. Also conditional if the user already is exsiting in the database or not

--- index.js
+++ index.js
@@ -28,10 +28,45 @@ const client = new MongoClient(uri, {
 
 async function run() {
    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      //   await client.connect();
-      // Send a ping to confirm a successful connection
-      //   await client.db("admin").command({ ping: 1 });
+      const usersCollection = client.db("flowTech").collection("users");
+
+      /***** USER RELATED APIs *****/
+      app.put("/users", async (req, res) => {
+         const user = req.body; // Extract user data from the request body
+         const query = { email: user?.email }; // Create a query object using the user's email
+
+         // Check if the user already exists in the database
+         const isExist = await usersCollection.findOne(query);
+         if (isExist) {
+            // If the user exists and is trying to change their status to "Requested"
+            if (user.status === "Requested") {
+               // Update the user's status in the database
+               const result = await usersCollection.updateOne(query, {
+                  $set: { status: user?.status },
+               });
+               return res.send(result); // Return the result of the update operation
+            } else {
+               // If the user exists and is not changing their status, return the existing user data
+               return res.send(isExist);
+            }
+         }
+
+         // If the user does not exist, save the user data for the first time
+         const options = { upsert: true }; // Set the upsert option to true
+         const updateDoc = {
+            $set: {
+               ...user, // Spread the user data into the update document
+               timestamp: Date.now(), // Add a timestamp field with the current date and time
+            },
+         };
+         // Perform the update operation with upsert option (inserts a new document if no matching document is found)
+         const result = await usersCollection.updateOne(
+            query,
+            updateDoc,
+            options
+         );
+         res.send(result); // Return the result of the update operation
+      });
 
       console.log(
          "Pinged your deployment. You successfully connected to MongoDB!"
